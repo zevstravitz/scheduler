43a6033a83cf672727dc13435ea1fe3ace28344e
Zev Stravitz <zevstravitz@verizon.net>
2020-04-09 16:33:58 -0400

    filters

--- src/App.js
+++ src/App.js
@@ -3,7 +3,13 @@ import 'rbx/index.css';
 import { Button, Container, Title } from 'rbx';
 
 
+
 const terms = { F: 'Fall', W: 'Winter', S: 'Spring'};
+
+
+const Banner = ({ title }) => (
+  <h1 className="title">{ title }</h1>
+);
 
 const getCourseTerm = course => (
   terms[course.id.charAt(0)]
@@ -13,21 +19,45 @@ const getCourseNumber = course => (
   course.id.slice(1, 4)
 )
 
+const buttonState = selected => (
+  selected ? `button is-success is-selected` : 'button'
+);
+
+const TermSelector = ({ state }) => (
+  <div className="field has-addons">
+  { Object.values(terms)
+      .map(value => 
+        <button key={value}
+          className={ buttonState(value === state.term) }
+          onClick={ () => state.setTerm(value) }
+          >
+          { value }
+        </button>
+      )
+  }
+  </div>
+);
+  
 const Course = ({ course }) => (
-  <Button>
+  <button className="button">
     { getCourseTerm(course) } CS { getCourseNumber(course) }: { course.title }
-  </Button>
+  </button>
 );
 
-const Banner = ({ title }) => (
-  <Title>{ title || '[loading...]' }</Title>
-);
-
-const CourseList = ({ courses }) => (
-  <Button.Group>
-    {courses.map(course => <Course key={course.id} course={ course } />)}
-  </Button.Group>
-);
+const CourseList = ({ courses }) => {
+  const [term, setTerm] = React.useState('Fall');
+  const termCourses = courses.filter(course => term === getCourseTerm(course));
+  
+  return (
+    <React.Fragment>
+      <TermSelector state={ { term, setTerm } } />
+      <div className="buttons">
+        { termCourses.map(course =>
+           <Course key={ course.id } course={ course }  />) }
+      </div>
+    </React.Fragment>
+  );
+};
 
 const App = () => {
   const [schedule, setSchedule] = useState({ title: '', courses: [] });
