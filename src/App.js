005b4c721f33c93505e370b4a0a962a174abd66e
Zev Stravitz <zevstravitz@verizon.net>
2020-04-09 14:40:33 -0400

    shows skeleton and loads classes

--- src/App.js
+++ src/App.js
@@ -1,32 +1,7 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import 'rbx/index.css';
 import { Button, Container, Title } from 'rbx';
 
-const schedule = {
-  "title": "CS Courses for 2018-2019",
-  "courses": [
-    {
-      "id": "F101",
-      "title": "Computer Science: Concepts, Philosophy, and Connections",
-      "meets": "MWF 11:00-11:50"
-    },
-    {
-      "id": "F110",
-      "title": "Intro Programming for non-majors",
-      "meets": "MWF 10:00-10:50"
-    },
-    {
-      "id": "F111",
-      "title": "Fundamentals of Computer Programming I",
-      "meets": "MWF 13:00-13:50"
-    },
-    {
-      "id": "F211",
-      "title": "Fundamentals of Computer Programming II",
-      "meets": "TuTh 12:30-13:50"
-    }
-  ]
-};
 
 const terms = { F: 'Fall', W: 'Winter', S: 'Spring'};
 
@@ -37,7 +12,7 @@ const getCourseTerm = course => (
 const getCourseNumber = course => (
   course.id.slice(1, 4)
 )
-  
+
 const Course = ({ course }) => (
   <Button>
     { getCourseTerm(course) } CS { getCourseNumber(course) }: { course.title }
@@ -45,7 +20,7 @@ const Course = ({ course }) => (
 );
 
 const Banner = ({ title }) => (
-  <Title>{ title }</Title>
+  <Title>{ title || '[loading...]' }</Title>
 );
 
 const CourseList = ({ courses }) => (
@@ -54,11 +29,27 @@ const CourseList = ({ courses }) => (
   </Button.Group>
 );
 
-const App = () =>  (
-  <Container>
-    <Banner title={ schedule.title } />
-    <CourseList courses={ schedule.courses } />
-  </Container>
-);
+const App = () => {
+  const [schedule, setSchedule] = useState({ title: '', courses: [] });
+
+  const url = 'https://courses.cs.northwestern.edu/394/data/cs-courses.php'
+
+  useEffect(() => {
+      const fetchSchedule = async () => {
+        const response = await fetch(url);
+        if (!response.ok) throw response;
+        const json = await response.json();
+        setSchedule(json);
+      }
+      fetchSchedule();
+  }, [])
+
+  return (
+    <Container>
+      <Banner title={ schedule.title } />
+      <CourseList courses={ schedule.courses } />
+    </Container>
+  )
+};
 
 export default App;
