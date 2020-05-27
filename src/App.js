a7deaa9100d6d84ee65a55df01cee1eb38054994
Zev Stravitz <zevstravitz@verizon.net>
2020-05-26 23:56:37 -0400

    end of 5

--- src/App.js
+++ src/App.js
@@ -1,11 +1,50 @@
 import React, { useState, useEffect } from 'react';
 import 'rbx/index.css';
-import { Button, Container, Title } from 'rbx';
+import { Container } from 'rbx';
+import firebase from 'firebase/app';
+import 'firebase/database';
 
+const firebaseConfig = {
+  apiKey: "AIzaSyCKooHGEn2sgo_W47r0uHDlTpumxpjBtU8",
+  authDomain: "quickreact-f0974.firebaseapp.com",
+  databaseURL: "https://quickreact-f0974.firebaseio.com",
+  projectId: "quickreact-f0974",
+  storageBucket: "quickreact-f0974.appspot.com",
+  messagingSenderId: "638982657911",
+  appId: "1:638982657911:web:e296a4b647c2f1ad3cf372"
+};
 
+firebase.initializeApp(firebaseConfig);
+const db = firebase.database().ref();
 
 const terms = { F: 'Fall', W: 'Winter', S: 'Spring'};
 
+// a conflict must involve overlapping days and times
+const days = ['M', 'Tu', 'W', 'Th', 'F'];
+
+const meetsPat = /^ *((?:M|Tu|W|Th|F)+) +(\d\d?):(\d\d) *[ -] *(\d\d?):(\d\d) *$/;
+
+const daysOverlap = (days1, days2) => ( 
+  days.some(day => days1.includes(day) && days2.includes(day))
+);
+
+const hoursOverlap = (hours1, hours2) => (
+  Math.max(hours1.start, hours2.start) < Math.min(hours1.end, hours2.end)
+);
+
+const timeConflict = (course1, course2) => (
+  daysOverlap(course1.days, course2.days) && hoursOverlap(course1.hours, course2.hours)
+);
+
+const courseConflict = (course1, course2) => (
+  course1 !== course2
+  && getCourseTerm(course1) === getCourseTerm(course2)
+  && timeConflict(course1, course2)
+);
+
+const hasConflict = (course, selected) => (
+  selected.some(selection => courseConflict(course, selection))
+);
 
 const Banner = ({ title }) => (
   <h1 className="title">{ title }</h1>
@@ -19,7 +58,7 @@ const getCourseNumber = course => (
   course.id.slice(1, 4)
 )
 
-const buttonState = selected => (
+const buttonColor = selected => (
   selected ? `button is-success is-selected` : 'button'
 );
 
@@ -28,7 +67,7 @@ const TermSelector = ({ state }) => (
   { Object.values(terms)
       .map(value => 
         <button key={value}
-          className={ buttonState(value === state.term) }
+          className={ buttonColor(value === state.term) }
           onClick={ () => state.setTerm(value) }
           >
           { value }
@@ -38,48 +77,81 @@ const TermSelector = ({ state }) => (
   </div>
 );
   
-const Course = ({ course }) => (
-  <button className="button">
+const Course = ({ course, state }) => (
+  <button className={ buttonColor(state.selected.includes(course)) }
+    onClick={ () => state.toggle(course) }
+    disabled={ hasConflict(course, state.selected) }
+    >
     { getCourseTerm(course) } CS { getCourseNumber(course) }: { course.title }
   </button>
 );
 
+const useSelection = () => {
+  const [selected, setSelected] = React.useState([]);
+  const toggle = (x) => {
+    setSelected(selected.includes(x) ? selected.filter(y => y !== x) : [x].concat(selected))
+  };
+  return [ selected, toggle ];
+};
+
 const CourseList = ({ courses }) => {
   const [term, setTerm] = React.useState('Fall');
+  const [selected, toggle] = useSelection();
   const termCourses = courses.filter(course => term === getCourseTerm(course));
-  
+ 
   return (
     <React.Fragment>
       <TermSelector state={ { term, setTerm } } />
       <div className="buttons">
         { termCourses.map(course =>
-           <Course key={ course.id } course={ course }  />) }
+           <Course key={ course.id } course={ course }
+             state={ { selected, toggle } } />) }
       </div>
     </React.Fragment>
   );
 };
 
+const timeParts = meets => {
+  const [match, days, hh1, mm1, hh2, mm2] = meetsPat.exec(meets) || [];
+  return !match ? {} : {
+    days,
+    hours: {
+      start: hh1 * 60 + mm1 * 1,
+      end: hh2 * 60 + mm2 * 1
+    }
+  };
+};
+
+const addCourseTimes = course => ({
+  ...course,
+  ...timeParts(course.meets)
+});
+
+const addScheduleTimes = schedule => ({
+  title: schedule.title,
+  courses: schedule.courses.map(addCourseTimes)
+});
+
 const App = () => {
-  const [schedule, setSchedule] = useState({ title: '', courses: [] });
+  const [schedule, setSchedule] = React.useState({ title: '', courses: [] });
+  const url = 'https://courses.cs.northwestern.edu/394/data/cs-courses.php';
 
-  const url = 'https://courses.cs.northwestern.edu/394/data/cs-courses.php'
-
-  useEffect(() => {
-      const fetchSchedule = async () => {
-        const response = await fetch(url);
-        if (!response.ok) throw response;
-        const json = await response.json();
-        setSchedule(json);
-      }
-      fetchSchedule();
+  React.useEffect(() => {
+    const fetchSchedule =  async () => {
+      const response = await fetch(url);
+      if (!response.ok) throw response;
+      const json = await response.json();
+      setSchedule(addScheduleTimes(json));
+    }
+    fetchSchedule();
   }, [])
 
   return (
-    <Container>
+    <div className="container">
       <Banner title={ schedule.title } />
       <CourseList courses={ schedule.courses } />
-    </Container>
-  )
+    </div>
+  );
 };
 
 export default App;
